1c01a1b9a7c139f94fefdfbb92597ca5a3691eac
Jonathan Gilmour <jonatgilmour@gmail.com>
2021-02-03 14:58:50 -0500

    scaled back and cleaned up

--- cms_id_cleanup.py
+++ cms_id_cleanup.py
@@ -1,18 +1,7 @@
 import pandas as pd
 import numpy as np
 
-#fl_processed_sheet = 'https://docs.google.com/spreadsheets/d/1UQRhavmfDFG77aQs1IWnudH8Oqx5DPWeUxXofOisXhc/gviz/tq?tqx=out:csv&sheet=Sheet1'
-fl_processed = '~/downloads/data (1).csv'
-fl_processed_df = pd.read_csv(fl_processed)
-
-cms_ltc_sheet = 'https://docs.google.com/spreadsheets/d/1d1PKh90_bkFhwgz3lSv3vKdqn4L8g0PM5qGMe75zBfM/gviz/tq?tqx=out:csv&sheet=test-facility-list-full'
-cms_df = pd.read_csv(cms_ltc_sheet)
-
-#renaming cms columns to those in the ltc sheet
-cms_df.columns.to_list()
-cms_df_trimmed = cms_df[['State ','City','Facility Name','County','CMS Facility ID','CTP Facility Categorization','State Facility Type']]
-cms_df_trimmed = cms_df_trimmed.rename(columns={'State ': 'State', 'Facility Name': 'Facility', 'CMS Facility ID': 'CMS_ID','CTP Facility Categorization': 'CTP_Facility_Type', 'State Facility Type': 'State_Facility_Type'})
-
+# Clean up CTP Facility Types and federal/state regulated. This is optimized for FL - other states have different labels
 def state_to_ctp(record):
     state = record['State_Facility_Type']
     if(state == 'ALF' or state == 'Assisted Living'):
@@ -29,15 +18,28 @@ def state_to_ctp(record):
         record['Regulate'] = None
     return record
 
-#make nan any CMS_IDs tied to facilities that are not nursing homes
+# Make null any CMS IDs tied to facilities that are not nursing homes. This is optimized for FL - other states have different labels
 def not_nh(record):
     if ((record['State_Facility_Type'] != 'NH') and ~(np.isnan(record['CMS_ID']))):
         record['CMS_ID'] = None
     return record
 
 # process
-# merge with cms
+# merge with cms - no longer
 # take new CMS id or old if no new
-# run state_to_ctp
+# run state_to_ctp (checks if state != ctp?)
 # run not_nh
 # clean, remove extraneous columns (new CMS)
+    
+def main():
+    # direct download not working?
+    #fl_processed_sheet = 'https://docs.google.com/spreadsheets/d/1UQRhavmfDFG77aQs1IWnudH8Oqx5DPWeUxXofOisXhc/gviz/tq?tqx=out:csv&sheet=Sheet1'
+    fl_processed = '~/downloads/data (1).csv'
+    fl_processed_df = pd.read_csv(fl_processed)
+    processed_with_type = fl_processed_df.apply(state_to_ctp, axis = 1)
+    processed_with_type = processed_with_type.apply(not_nh, axis = 1)
+    processed_with_type.to_csv('fl_facilities.csv')
+
+if __name__ == "__main__":
+    main()
+
