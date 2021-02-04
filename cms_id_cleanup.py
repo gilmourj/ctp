fac79cd0d30eed16714a065d667c19db9fac6e4d
Jonathan Gilmour <jonatgilmour@gmail.com>
2021-02-03 21:37:51 -0500

    final version of fl cleanup functions

--- cms_id_cleanup.py
+++ cms_id_cleanup.py
@@ -14,21 +14,22 @@ def state_to_ctp(record):
         record['CTP_Facility_Type'] = 'Other'
         record['Regulate'] = 'State'
     else:
-        record['CTP_Facility_Type'] = None
-        record['Regulate'] = None
+        record['CTP_Facility_Type'] = np.nan
+        record['Regulate'] = np.nan
     return record
 
 # Make null any CMS IDs tied to facilities that are not nursing homes. This is optimized for FL - other states have different labels
 def not_nh(record):
-    if ((record['State_Facility_Type'] != 'NH') and ~(np.isnan(record['CMS_ID']))):
-        record['CMS_ID'] = None
+    if ((record['State_Facility_Type'] != 'NH') and (not pd.isnull(record['CMS_ID']))):
+        import pdb; pdb.set_trace()
+        record['CMS_ID'] = np.nan
     return record
     
 def main():
     fl_processed_sheet = 'https://docs.google.com/spreadsheets/d/1UQRhavmfDFG77aQs1IWnudH8Oqx5DPWeUxXofOisXhc/gviz/tq?tqx=out:csv&sheet=Sheet1'
-    fl_processed_df = pd.read_csv(fl_processed_sheet)
+    fl_processed_df = pd.read_csv(fl_processed)
     processed_with_type = fl_processed_df.apply(state_to_ctp, axis = 1)
-    processed_with_type = processed_with_type.apply(not_nh, axis = 1)
+    processed_with_type = fl_processed_df.apply(not_nh, axis = 1)
     processed_with_type.to_csv('fl_facilities.csv')
 
 if __name__ == "__main__":
